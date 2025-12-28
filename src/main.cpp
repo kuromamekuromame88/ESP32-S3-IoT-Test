eee98a3fe2ac08bf67da3d3e883fee33097c751d
kuromamekuromame88 <tei8sia@outlook.jp>
2025-12-28 18:08:49 +0900

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
-#include <ledc.h>
 
 #define PWM_PIN 5
 #define FREQUENCY 1000
 
+/*
 int state = false;
 
 void ledon(int level){
@@ -19,19 +19,21 @@ void ledoff(int level){
 
 bool getstate(){
   return state;
-}
+}*/
 
 void setup(){
   //ピンの設定
   pinMode(PWM_PIN, OUTPUT);
 
   //PWM設定（ピン番号, 周波数, 分解能）
-  ledcAttach(PWM_PIN, FREQUENCY, 8);
+  //ledcAttach(PWM_PIN, FREQUENCY, 8);
 }
 
 void loop(){
-  ledcWrite(PWM_PIN, 0);
+  digitalWrite(PWM_PIN, LOW);
+  //ledcWrite(PWM_PIN, 0);
   delay(1000);
-  ledcWrite(PWM_PIN, 255);
+  digitalWrite(PWM_PIN, HIGH);
+  //ledcWrite(PWM_PIN, 255);
   delay(1000);
 }
