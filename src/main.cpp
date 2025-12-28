f124c8e67a3704668235f31f725ab2e40d41a153
kuromamekuromame88 <tei8sia@outlook.jp>
2025-12-28 19:39:16 +0900

    a

--- src/main.cpp
+++ src/main.cpp
@@ -101,138 +101,137 @@ const uint32_t decommissioningTimeout = 5000; // keep the button pressed for 5s,
 
 // Set the RGB LED Light based on the current state of the Color Light
 bool setLightState(bool state, espHsvColor_t colorHSV) {
-
-if (state) {
-#ifdef RGB_BUILTIN
-espRgbColor_t rgbColor = espHsvColorToRgbColor(colorHSV);
-// set the RGB LED
-rgbLedWrite(ledPin, rgbColor.r, rgbColor.g, rgbColor.b);
-#else
-// No Color RGB LED, just use the HSV value (brightness) to control the LED
-analogWrite(ledPin, colorHSV.v);
-#endif
-} else {
-#ifndef RGB_BUILTIN
-// after analogWrite(), it is necessary to set the GPIO to digital mode first
-pinMode(ledPin, OUTPUT);
-#endif
-digitalWrite(ledPin, LOW);
-}
-// store last HSV Color and OnOff state for when the Light is restarted / power goes off
-matterPref.putBool(onOffPrefKey, state);
-matterPref.putUInt(hsvColorPrefKey, colorHSV.h << 16 | colorHSV.s << 8 | colorHSV.v);
-// This callback must return the success state to Matter core
-return true;
+  if (state) {
+    #ifdef RGB_BUILTIN
+    espRgbColor_t rgbColor = espHsvColorToRgbColor(colorHSV);
+    // set the RGB LED
+    rgbLedWrite(ledPin, rgbColor.r, rgbColor.g, rgbColor.b);
+    #else
+    // No Color RGB LED, just use the HSV value (brightness) to control the LED
+    analogWrite(ledPin, colorHSV.v);
+    #endif
+  } else {
+  #ifndef RGB_BUILTIN
+  // after analogWrite(), it is necessary to set the GPIO to digital mode first
+  pinMode(ledPin, OUTPUT);
+  #endif
+    digitalWrite(ledPin, LOW);
+  }
+  // store last HSV Color and OnOff state for when the Light is restarted / power goes off
+  matterPref.putBool(onOffPrefKey, state);
+  matterPref.putUInt(hsvColorPrefKey, colorHSV.h << 16 | colorHSV.s << 8 | colorHSV.v);
+  // This callback must return the success state to Matter core
+  return true;
 }
 
 void setup() {
-// Initialize the USER BUTTON (Boot button) GPIO that will act as a toggle switch
-pinMode(buttonPin, INPUT_PULLUP);
-// Initialize the LED (light) GPIO and Matter End Point
-pinMode(ledPin, OUTPUT);
+  // Initialize the USER BUTTON (Boot button) GPIO that will act as a toggle switch
+  pinMode(buttonPin, INPUT_PULLUP);
+  // Initialize the LED (light) GPIO and Matter End Point
+  pinMode(ledPin, OUTPUT);
 
-Serial.begin(115200);
+  Serial.begin(115200);
 
-// We start by connecting to a WiFi network
-Serial.print("Connecting to ");
-Serial.println(ssid);
-// Manually connect to WiFi
-WiFi.begin(ssid, password);
-// Wait for connection
-while (WiFi.status() != WL_CONNECTED) {
-delay(500);
-Serial.print(".");
-}
-Serial.println("\r\nWiFi connected");
-Serial.println("IP address: ");
-Serial.println(WiFi.localIP());
-delay(500);
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  // Manually connect to WiFi
+  WiFi.begin(ssid, password);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\r\nWiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(500);
 
-// Initialize Matter EndPoint
-matterPref.begin("MatterPrefs", false);
-// default OnOff state is ON if not stored before
-bool lastOnOffState = matterPref.getBool(onOffPrefKey, true);
-// default HSV color is blue HSV(169, 254, 254)
-uint32_t prefHsvColor = matterPref.getUInt(hsvColorPrefKey, 169 << 16 | 254 << 8 | 254);
-espHsvColor_t lastHsvColor = {uint8_t(prefHsvColor >> 16), uint8_t(prefHsvColor >> 8), uint8_t(prefHsvColor)};
-ColorLight.begin(lastOnOffState, lastHsvColor);
-// set the callback function to handle the Light state change
-ColorLight.onChange(setLightState);
+  // Initialize Matter EndPoint
+  matterPref.begin("MatterPrefs", false);
+  // default OnOff state is ON if not stored before
+  bool lastOnOffState = matterPref.getBool(onOffPrefKey, true);
+  // default HSV color is blue HSV(169, 254, 254)
+  uint32_t prefHsvColor = matterPref.getUInt(hsvColorPrefKey, 169 << 16 | 254 << 8 | 254);
+  espHsvColor_t lastHsvColor = {uint8_t(prefHsvColor >> 16), uint8_t(prefHsvColor >> 8), uint8_t(prefHsvColor)};
+  ColorLight.begin(lastOnOffState, lastHsvColor);
+  // set the callback function to handle the Light state change
+  ColorLight.onChange(setLightState);
 
-// lambda functions are used to set the attribute change callbacks
-ColorLight.onChangeOnOff([](bool state) {
-Serial.printf("Light OnOff changed to %s\r\n", state ? "ON" : "OFF");
-return true;
-});
-ColorLight.onChangeColorHSV([](HsvColor_t hsvColor) {
-Serial.printf("Light HSV Color changed to (%d,%d,%d)\r\n", hsvColor.h, hsvColor.s, hsvColor.v);
-return true;
-});
+  // lambda functions are used to set the attribute change callbacks
+  ColorLight.onChangeOnOff([](bool state) {
+    Serial.printf("Light OnOff changed to %s\r\n", state ? "ON" : "OFF");
+    return true;
+  });
+  ColorLight.onChangeColorHSV([](HsvColor_t hsvColor) {
+    Serial.printf("Light HSV Color changed to (%d,%d,%d)\r\n", hsvColor.h, hsvColor.s, hsvColor.v);
+    return true;
+  });
 
-// Matter beginning - Last step, after all EndPoints are initialized
-Matter.begin();
-// This may be a restart of a already commissioned Matter accessory
-if (Matter.isDeviceCommissioned()) {
-Serial.println("Matter Node is commissioned and connected to Wi-Fi. Ready for use.");
-Serial.printf(
-"Initial state: %s | RGB Color: (%d,%d,%d) \r\n", ColorLight ? "ON" : "OFF", ColorLight.getColorRGB().r, ColorLight.getColorRGB().g,
-ColorLight.getColorRGB().b
-);
-// configure the Light based on initial on-off state and its color
-ColorLight.updateAccessory();
-}
+  // Matter beginning - Last step, after all EndPoints are initialized
+  Matter.begin();
+  // This may be a restart of a already commissioned Matter accessory
+  if (Matter.isDeviceCommissioned()) {
+    Serial.println("Matter Node is commissioned and connected to Wi-Fi. Ready for use.");
+    Serial.printf(
+      "Initial state: %s | RGB Color: (%d,%d,%d) \r\n", ColorLight ? "ON" : "OFF", ColorLight.getColorRGB().r, ColorLight.getColorRGB().g,
+      ColorLight.getColorRGB().b
+    );
+    // configure the Light based on initial on-off state and its color
+    ColorLight.updateAccessory();
+  }
 }
 
 void loop() {
-// Check Matter Light Commissioning state, which may change during execution of loop()
-if (!Matter.isDeviceCommissioned()) {
-Serial.println("");
-Serial.println("Matter Node is not commissioned yet.");
-Serial.println("Initiate the device discovery in your Matter environment.");
-Serial.println("Commission it to your Matter hub with the manual pairing code or QR code");
-Serial.printf("Manual pairing code: %s\r\n", Matter.getManualPairingCode().c_str());
-Serial.printf("QR code URL: %s\r\n", Matter.getOnboardingQRCodeUrl().c_str());
-// waits for Matter Light Commissioning.
-uint32_t timeCount = 0;
-while (!Matter.isDeviceCommissioned()) {
-delay(100);
-if ((timeCount++ % 50) == 0) { // 50*100ms = 5 sec
-Serial.println("Matter Node not commissioned yet. Waiting for commissioning.");
-}
-}
-Serial.printf(
-"Initial state: %s | RGB Color: (%d,%d,%d) \r\n", ColorLight ? "ON" : "OFF", ColorLight.getColorRGB().r, ColorLight.getColorRGB().g,
-ColorLight.getColorRGB().b
-);
-// configure the Light based on initial on-off state and its color
-ColorLight.updateAccessory();
-Serial.println("Matter Node is commissioned and connected to Wi-Fi. Ready for use.");
-}
+  // Check Matter Light Commissioning state, which may change during execution of loop()
+  if (!Matter.isDeviceCommissioned()) {
+    Serial.println("");
+    Serial.println("Matter Node is not commissioned yet.");
+    Serial.println("Initiate the device discovery in your Matter environment.");
+    Serial.println("Commission it to your Matter hub with the manual pairing code or QR code");
+    Serial.printf("Manual pairing code: %s\r\n", Matter.getManualPairingCode().c_str());
+    Serial.printf("QR code URL: %s\r\n", Matter.getOnboardingQRCodeUrl().c_str());
+    // waits for Matter Light Commissioning.
+    uint32_t timeCount = 0;
+    while (!Matter.isDeviceCommissioned()) {
+      delay(100);
+      if ((timeCount++ % 50) == 0) { // 50*100ms = 5 sec
+        Serial.println("Matter Node not commissioned yet. Waiting for commissioning.");
+      }
+    }
+    Serial.printf(
+      "Initial state: %s | RGB Color: (%d,%d,%d) \r\n", ColorLight ? "ON" : "OFF", ColorLight.getColorRGB().r, ColorLight.getColorRGB().g,
+      ColorLight.getColorRGB().b
+    );
+    // configure the Light based on initial on-off state and its color
+    ColorLight.updateAccessory();
+    Serial.println("Matter Node is commissioned and connected to Wi-Fi. Ready for use.");
+  }
 
-// A button is also used to control the light
-// Check if the button has been pressed
-if (digitalRead(buttonPin) == LOW && !button_state) {
-// deals with button debouncing
-button_time_stamp = millis(); // record the time while the button is pressed.
-button_state = true; // pressed.
-}
+  // A button is also used to control the light
+  // Check if the button has been pressed
+  if (digitalRead(buttonPin) == LOW && !button_state) {
+    // deals with button debouncing
+    button_time_stamp = millis(); // record the time while the button is pressed.
+    button_state = true; // pressed.
+  }
 
-// Onboard User Button is used as a Light toggle switch or to decommission it
-uint32_t time_diff = millis() - button_time_stamp;
-if (digitalRead(buttonPin) == HIGH && button_state && time_diff > debouceTime) {
-// Toggle button is released - toggle the light
-Serial.println("User button released. Toggling Light!");
-ColorLight.toggle(); // Matter Controller also can see the change
-button_state = false; // released
-}
+  // Onboard User Button is used as a Light toggle switch or to decommission it
+  uint32_t time_diff = millis() - button_time_stamp;
+  if (digitalRead(buttonPin) == HIGH && button_state && time_diff > debouceTime) {
+    // Toggle button is released - toggle the light
+    Serial.println("User button released. Toggling Light!");
+    ColorLight.toggle(); // Matter Controller also can see the change
+    button_state = false; // released
+  }
 
-// Onboard User Button is kept pressed for longer than 5 seconds in order to decommission matter node
-if (button_state && time_diff > decommissioningTimeout) {
-Serial.println("Decommissioning the Light Matter Accessory. It shall be commissioned again.");
-ColorLight = false; // turn the light off
-Matter.decommission();
-button_time_stamp = millis(); // avoid running decommissining again, reboot takes a second or so
-}
+  // Onboard User Button is kept pressed for longer than 5 seconds in order to decommission matter node
+  if (button_state && time_diff > decommissioningTimeout) {
+    Serial.println("Decommissioning the Light Matter Accessory. It shall be commissioned again.");
+    ColorLight = false; // turn the light off
+    Matter.decommission();
+    button_time_stamp = millis(); // avoid running decommissining again, reboot takes a second or so
+  }
 }
 
 #endif
