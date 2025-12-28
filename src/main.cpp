98842a7fb34f0ceb00acd5bc76886cb38c00c0e4
kuromamekuromame88 <tei8sia@outlook.jp>
2025-12-28 22:40:15 +0900

    Fix typo in JSON deserialization function

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
       USBSerial.println(jsonStr);
 
       StaticJsonDocument<512> doc;
-      DeSerializationError err = deUSBSerializeJson(doc, jsonStr);
+      DeserializationError err = deserializeJson(doc, jsonStr);
 
       if (err) {
         USBSerial.print("[JSON] Parse error: ");
